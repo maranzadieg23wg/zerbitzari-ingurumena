079b75def1203979d75f6b160a9d4eb71ec22ddf
Manex Aranzadi Egaña <maranzadieg23wg@ikzubirimanteo.com>
2024-09-23 10:20:15 +0000

    Idatzitako datuak ongi agertzen dira

--- 1.Hirulekoa/4.Formulario/src/form.php
+++ 1.Hirulekoa/4.Formulario/src/form.php
@@ -1,2 +1,7 @@
 Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+Your email address is: <?php echo $_POST["email"]; ?><br>
+Your password is: <?php echo $_POST["password"]; ?><br>
+Your website is: <?php echo $_POST["website"]; ?><br>
+Your comment is: <?php echo $_POST["comment"]; ?><br>
+Your gender is: <?php echo $_POST["gender"]; ?><br>
+Your option was: <?php echo $_POST["taskOption"]; ?><br>
