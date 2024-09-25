a59c5b4a7876eb7dc9c9a5dc2cc9fe16b6bdd390
Manex Aranzadi Egaña <maranzadieg23wg@ikzubirimanteo.com>
2024-09-25 07:21:21 +0000

    loggin bukatuta

--- 1.Hirulekoa/5.Sessions/src/logging.php
+++ 1.Hirulekoa/5.Sessions/src/logging.php
@@ -1,10 +1,11 @@
 <?php
+ob_start();
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
 
     // Begiratzen da ea gmaila ongi dagoen a la ez
     function validateEmail($email) {
-        return !empty($name) && filter_var($email, FILTER_VALIDATE_EMAIL);
+        return !empty($email) && filter_var($email, FILTER_VALIDATE_EMAIL);
     }
 
     
@@ -14,6 +15,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $password = htmlspecialchars($_POST["password"]);
     $hash = password_hash($password, PASSWORD_DEFAULT);
 
+    //echo($hash);
 
 
     // Balidazioa
@@ -30,14 +32,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             echo "<p style='color:red;'>$error</p>";
         }
     } else {
-        // Guztia ongi egonez gero, erakutsiko du
-        echo "Welcome $name <br>";
-        echo "Your email address is: $email <br>";
-        echo "Your password is: $password <br>";
-        echo "Your website is: $website <br>";
-        echo "Your comment is: $comment <br>";
-        echo "Your gender is: $gender <br>";
-        echo "Your option was: $taskOption <br>";
+
+        $hashedPassword = '$2y$10$mVUx2BHbrqssqNW1xp.YauqeQxQeBXDdW8zLnXNDMoJ.4b5HCu6Wi';
+
+        if ($email == "admin@admin.com" && password_verify($password, $hashedPassword)){
+            // Guztia ongi egonez gero, erakutsiko du
+            echo "Welcome $email <br>";
+
+            setcookie("erabiltzailea", $email."@".$hashedPassword, 0);
+            header('Location:admin.php');
+            ob_end_flush();
+        }else{
+            echo "Emaila edo pasahitza gaizki sartuta";
+        }
+        
+        
     }
 }
 ?>
