2e34164b4dbd8b2b8462ebdc9c71e572694b5b9b
Manex Aranzadi Egaña <maranzadieg23wg@ikzubirimanteo.com>
2024-09-25 07:55:55 +0000

    Admin eginda

--- 1.Hirulekoa/5.Sessions/src/admin.php
+++ 1.Hirulekoa/5.Sessions/src/admin.php
@@ -1,6 +1,55 @@
 <?php 
+//ob_start();
 
-echo ("hellow word");
+
+
+$hashedPassword = '$2y$10$mVUx2BHbrqssqNW1xp.YauqeQxQeBXDdW8zLnXNDMoJ.4b5HCu6Wi';
+$email = "admin@admin.com";
+
+
+if (isset($_COOKIE["erabiltzailea"])) {
+    if ($_COOKIE["erabiltzailea"] !== $email) {
+        header('Location: index.html');
+        exit();
+    }
+} else {
+    header('Location: index.html');
+    exit();
+}
+
+
+//echo "Hellow ".$_COOKIE["erabiltzailea"];
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['logout'])) {
+   
+    setcookie("erabiltzailea", "", time() - 3600, "/");
+    header('Location: index.html');
+    exit();
+}
 
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manex Aranzadi Egaña</title>
+</head>
+<body>
+
+
+
+    <h1>Ongi etorri admin horrialdera</h1>
+    <form method="POST">
+        <button type="submit" name="logout">Saioa itxi</button>
+    </form>
+
+
+
+
+</body>
+</html>
