418747b5e6f880207210f08108f614714efe0750
Manex Aranzadi Egaña <maranzadieg23wg@ikzubirimanteo.com>
2024-09-30 11:07:31 +0000

    Config fitxategia eginda. Konektatzeko DB-ra

--- 1.Hirulekoa/5.Sessions/src/config.php
+++ 1.Hirulekoa/5.Sessions/src/config.php
@@ -1,10 +1,15 @@
 <?php
-// Basic connection settings
-$databaseHost = '10.14.0.2';
-$databaseUsername = 'root';
-$databasePassword = 'root';
-$databaseName = 'mydatabase';
+$servername = "10.14.0.2:8306";  // Nombre del contenedor MariaDB en Docker
+$username = "root";
+$password = "root";
+$dbname = "mydatabase";
 
-// Connect to the database
-$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+//echo "Connected successfully to MariaDB!";
 ?>
