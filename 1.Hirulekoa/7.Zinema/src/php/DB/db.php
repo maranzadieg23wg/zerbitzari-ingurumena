da0bb90bca9fdeaa7ba496edff05c915759e8061
Manex Aranzadi Egaña <maranzadieg23wg@ikzubirimanteo.com>
2024-10-14 11:00:43 +0000

    DB

---
+++ 1.Hirulekoa/7.Zinema/src/php/DB/db.php
@@ -0,0 +1,48 @@
+<?php
+require_once './config.php';
+
+class UserManager {
+    private $db; 
+    private $conn;
+
+    
+    public function __construct() {
+        
+        $this->conn = $this->db->getConnection();
+    }
+
+    public function getAllUsers() {
+        $sql = "SELECT * FROM users";
+        $result = $this->conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            $users = [];
+            while ($row = $result->fetch_assoc()) {
+                $users[] = $row;
+            }
+            return $users;
+        } else {
+            return [];
+        }
+    }
+
+    public function getFilms() {
+        $sql = "SELECT * FROM films ORDER BY RAND() LIMIT 5;";
+        $result = $this->conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            $films = [];
+            while ($row = $result->fetch_assoc()) {
+                $films[] = $row;
+            }
+            return $films;
+        } else {
+            return [];
+        }
+    }
+
+    public function close() {
+        $this->db->closeConnection();
+    }
+}
+?>
