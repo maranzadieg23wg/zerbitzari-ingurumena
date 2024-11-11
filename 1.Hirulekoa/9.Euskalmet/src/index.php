2aef85a903dc0b0b212d314544c5a3aa4f157056
Manex Aranzadi Egaña <maranzadieg23wg@ikzubirimanteo.com>
2024-11-11 11:50:30 +0000

    egunak erakutsituta

--- 1.Hirulekoa/9.Euskalmet/src/index.php
+++ 1.Hirulekoa/9.Euskalmet/src/index.php
@@ -1,5 +1,5 @@
 <?php
-        include 'php/headerFooter/header.php';
+        // include 'php/headerFooter/header.php';
 ?>
 
 <style>
@@ -11,22 +11,65 @@
 <body>
 
     <div id="">
-        
+
         <?php
             require_once 'php/DB/db.php';
 
             $db = new UserManager();
+
             $herriLista = $db->getAllHerriak();
+
+            $egunak = [];
+
+            
+            if (isset($_POST['herriak'])) {
+                $herri_id = $_POST['herriak'];
+                $egunak = $db->getEguraldia($herri_id); 
+                
+            }
         ?>
 
-        <select id="herriak" name="herriak">
-            <?php 
-                foreach ($herriLista as $herri) { 
-                    echo "<option value=\"{$herri['id']}\">{$herri['izena']}</option>";
+        <form action="" method="POST">
+            <select id="herriak" name="herriak" onchange="this.form.submit()">
+                <?php
+
+                foreach ($herriLista as $herri) {
+                    $selected = (isset($herri_id) && $herri_id == $herri['id']) ? 'selected' : '';
+                    echo "<option value=\"{$herri['id']}\" $selected>{$herri['izena']}</option>";
                 }
-            ?>
-        </select>
+                ?>
+            </select>
+        </form>
+
+        <?php if (!empty($egunak)): ?>
+            <table style="width:100%; margin-top: 20px; border: 1px solid #000;text-align: center;">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Fecha</th>
+                        <th>Estado del clima</th>
+                        <th>Temperatura mínima</th>
+                        <th>Temperatura máxima</th>
+                        <th>Aukeratu Eguna</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($egunak as $fila): ?>
+                        <tr>
+                            <td><?php echo $fila['id']; ?></td>
+                            <td><?php echo $fila['eguna']; ?></td>
+                            <td><?php echo $fila['eguraldia']; ?></td>
+                            <td><?php echo $fila['tenperatura_min']; ?></td>
+                            <td><?php echo $fila['tenperatura_max']; ?></td>
+                            <td><?php echo "input type" ?></td>
+
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        <?php endif; ?>
 
     </div>
-    
+
 </body>
--- 1.Hirulekoa/9.Euskalmet/src/php/DB/db.php
+++ 1.Hirulekoa/9.Euskalmet/src/php/DB/db.php
@@ -18,7 +18,7 @@ class UserManager {
 
         $this -> open();
 
-        $sql = "SELECT * FROM herria";
+        $sql = "SELECT * FROM herria ORDER BY izena";
         $result = $this->conn->query($sql);
 
         $this -> close();
@@ -31,330 +31,31 @@ class UserManager {
             return $herriak;
         } else {
             return [];
-        }
-
-        
+        }        
     }
 
-    // ******************************PELIKULAK****************************** //
 
-    public function getFilms() {
+    public function getEguraldia($id) {
 
         $this -> open();
 
-        $sql = "SELECT * FROM films ORDER BY RAND() LIMIT 4;";
+        $sql = "SELECT * FROM iragarpen_eguna where herria = $id ORDER BY eguna";
         $result = $this->conn->query($sql);
 
         $this -> close();
 
         if ($result->num_rows > 0) {
-            $films = [];
+            $herriak = [];
             while ($row = $result->fetch_assoc()) {
-                $films[] = $row;
+                $herriak[] = $row;
             }
-            return $films;
+            return $herriak;
         } else {
             return [];
-        }
-
-        
+        }        
     }
 
-    public function getFilmID($ID) {
-
-        $this -> open();
-
-        $sql = "SELECT * FROM films where Id = '$ID';";
-        $result = $this->conn->query($sql);
-
-        $this -> close();
-
-        if ($result->num_rows > 0) {
-            
-            return $result->fetch_assoc();
-        } else {
-            return null;
-        }
-
-        
-    }
-
-    public function getFilmName($name) {
-
-        $this -> open();
-
-        $sql = "SELECT * FROM films where name = '$name';";
-        $result = $this->conn->query($sql);
-
-        $this -> close();
-
-        if ($result->num_rows > 0) {
-            
-            return $result->fetch_assoc();
-        } else {
-            return null;
-        }
-
-        
-    }
-
-    public function getFilmISAN($isan) {
-
-        $this -> open();
-
-        $sql = "SELECT * FROM films where ISAN = '$isan';";
-        $result = $this->conn->query($sql);
-
-        $this -> close();
-
-        if ($result->num_rows > 0) {
-            
-            return $result->fetch_assoc();
-        } else {
-            return null;
-        }
-
-        
-    }
-
-
-    public function getMedia($ID){
-        $this -> open();
-
-        $sql = "SELECT AVG(nota) AS media FROM puntuazioa where film_id = '$ID';";
-        $result = $this->conn->query($sql);
-
-        $this -> close();
-
-        if ($result->num_rows > 0) {
-            $row = $result->fetch_assoc(); 
-            $media = $row['media']; 
-            return $media;
-        } else {
-            $media = null;  
-            return 0;
-        }
-    }
-
-    public function getUserNota($filmID, $userID){
-        $this -> open();
-
-        $sql = "SELECT nota FROM puntuazioa where film_id = '$filmID' and user_id ='$userID';";
-        $result = $this->conn->query($sql);
-
-        $this -> close();
-
-        if ($result->num_rows > 0) {
-            $row = $result->fetch_assoc(); 
-            $media = $row['nota']; 
-            return $media;
-        } else {
-            $media = null;  
-            return '-';
-        }
-    }
-
-
-    public function getUserFilms($filmID, $userID){
-        $this -> open();
-
-        $sql = "SELECT * FROM f.films where film_id = '$filmID' and user_id ='$userID';";
-        $result = $this->conn->query($sql);
-
-        $this -> close();
-
-        if ($result->num_rows > 0) {
-            $row = $result->fetch_assoc(); 
-            $media = $row['nota']; 
-            return $media;
-        } else {
-            $media = null;  
-            return '-';
-        }
-    }
-
-    // ******************************NOTAK****************************** //
-
-    public function lortuNota($idBezero, $idPelikula){
-
-        $this -> open();
-
-        $sql = "SELECT * FROM puntuazioa WHERE user_id = '$idBezero' and film_id = '$idPelikula';";
-        $result = $this->conn->query($sql);
-
-        $this -> close();
-    }
-
-    public function setNota($idBezero, $idPelikula, $nota){
-        $this -> open();
-
-        $sql = "SELECT * FROM puntuazioa WHERE user_id = '$idBezero' and film_id = '$idPelikula';";
-        $result = $this->conn->query($sql);
-
-        $this -> close();
-
-        if ($result->num_rows > 0) {
-
-            $this -> open();
-
-            $sql = "UPDATE puntuazioa SET nota = $nota WHERE user_id = '$idBezero' and film_id = '$idPelikula';";
-            $result = $this->conn->query($sql);
-
-            $this -> close();
-        } else {
-            $this -> notaSortu($idBezero, $idPelikula, $nota);
-        }
-    }
-
-    public function notaSortu($idBezero, $idPelikula, $nota){
-        $this -> open();
-
-        $sql = "INSERT INTO puntuazioa (user_id, film_id, nota) VALUES ($idBezero, $idPelikula, $nota)";
-        $result = $this->conn->query($sql);
-
-        $this -> close();
-
-        return true;
-        if ($result->num_rows > 0) {
-
-
-            
-            
-        } else {
-            return false;
-        }
-    }
-
-    // ******************************BILATZ****************************** //
-
-    public function findName($filmName){
-        $this -> open();
-
-        $sql = "SELECT * FROM films WHERE name like '$filmName';";
-        $result = $this->conn->query($sql);
-
-        $this -> close();
-
-        if($result ->num_rows > 0){
-            $films = [];
-            while ($row = $result->fetch_assoc()) {
-                $films[] = $row;
-            }
-            return $films;
-        }
-
-        return $this-> getFilmISAN($filmName);
-    }
-
-
-    // ******************************ERABILTZAILEAK****************************** //
-    public function userExist($username, $email){
-        $this -> open();
-
-        $sql = "SELECT * FROM users WHERE email = '$email' OR username = '$username';";
-        $result = $this->conn->query($sql);
-
-        $this -> close();
-
-        if($result ->num_rows > 0){
-            return true;
-        }else{
-            return false;
-        }
-
-
-    }
-
-
-    public function createUser($username, $email, $password, $img){
-        if(!$this->userExist($username, $email)){
-
-            $hashPassword = hash('sha256', $password);
-            $this -> open();
-
-            $sql = "INSERT INTO users (username, email, password, avatar) VALUES ('$username', '$email', '$hashPassword', '$img');";
-            $result = $this->conn->query($sql);
-
-            $this -> close();
-
-            
-
-            if($result ->num_rows > 0){
-                return true;
-            }else{
-                return false;
-            }
-
-        }else{
-            return false;
-        }
-    }
-
-    
-
-    public function logginSession() {
-        session_start();
-    
-        if (isset($_SESSION['sesioa'])) {
-            $sesioa = $_SESSION['sesioa'];
-    
-            $this->open();
-            $sql = "SELECT * FROM users WHERE ID = '$sesioa';";
-            $result = $this->conn->query($sql);
-            $this->close();
-    
-            if ($result->num_rows > 0) {
-                return $result->fetch_assoc();
-            } else {
-                return null;
-            }
-        } else {
-            return null;
-        }
-    }
-    
-
-    public function loggin($email, $password){
-        
-        
-        /*if($this->logginSession() !=null){
-            return $this->logginSession();
-        }*/
-
-        if($this->userExist(" ", $email)){
-
-            //echo "<script>console.log('ezistitzen da 2');</script>";
-            $hashPassword = hash('sha256', $password);
-            $this -> open();
-
-            $sql = "SELECT * FROM users WHERE (email = '$email') and (password = '$hashPassword');";
-            $result = $this->conn->query($sql); 
-
-            $this -> close();
-    
-            if($result ->num_rows > 0){
-                
-                $a = $result->fetch_assoc();
-                echo "<script>console.log('ezistitzen da 2');</script>";
-
-                ob_start();
-                //session_start();
-                $_SESSION['sesioa'] = json_encode($a);
-                ob_end_flush();
-
-                
-                return true;
-                //return $result->fetch_assoc();
-            }else{
-                return false;
-            }
-
-            
-        }
-
-        return false;
-
-    }
+    /***********************OPEN-CLOSE********************************/
 
     public function close() {
         $this->db->closeConnection();
